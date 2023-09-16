065c221c531abd7fa48f92cc9bddb1e763f824a1
emerson-oliveira-clear <emerson.oliveira.clear@gmail.com>
2023-09-16 13:44:57 -0300

    ignore

---
+++ Logica de programação/01 - Operadores de comparação/index.js
@@ -0,0 +1,25 @@
+
+/*
+> - maior
+< - menor
+>= - maior ou igual
+<= - menor ou igual
+== - igual
+=== - estritamente igual (valor e tipo)
+!= - diferente
+!== - estritamente diferente (valor e tipo)
+*/
+
+console.log(10 > 5)
+console.log(10 < 5)
+console.log(10 >= 5)
+console.log(10 <= 5)
+console.log(10 == 5)
+console.log(10 === 5)
+console.log(10 != 5)
+console.log(10 !== 5)
+
+let data2 = new Date()
+
+console.log(data2 = new Date())
+
