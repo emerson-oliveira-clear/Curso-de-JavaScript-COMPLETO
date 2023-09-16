689c8f5d70bcf3391613c9bdcdfb75aa6c718e33
emerson-oliveira-clear <emerson.oliveira.clear@gmail.com>
2023-09-16 13:45:46 -0300

    UPDATE: examples of logical operators

---
+++ Logica de programação/02 - Operadores Lógicos/index.js
@@ -0,0 +1,26 @@
+
+/*
+
+&& - AND/E
+|| - OR/OU
+! - NOT/NÃO
+
+*/
+
+const nota1 = 8
+const nota2 = 6
+
+if(nota1 >= 6 && nota2 >= 6){
+    console.log('Aprovado')
+} 
+
+if(nota1 >= 6 || nota2 >= 6){
+    console.log('Aprovado')
+} 
+
+if(nota1 != 6 || nota2 != 5){
+    console.log('Aprovado')
+} 
+
+
+
