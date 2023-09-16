3c2aceaf135b1afcf146503aa3b82dadd1ef3386
emerson-oliveira-clear <emerson.oliveira.clear@gmail.com>
2023-09-16 13:50:12 -0300

    UPDATE: Created CSS style for files

--- Logica de programação/if else/index.js
+++
@@ -1,9 +0,0 @@
-const hora = 3
-
-if(hora >= 6 && hora <= 11){
-    console.log('Bommm diaa!!');
-} else if(hora >= 12 && hora <= 18){
-    console.log('Boaaa tardee!!');
-}else {
-    console.log('Boaaa noiteee!!');
-}
