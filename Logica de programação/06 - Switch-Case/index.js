5e1bdf48926e0b5c3451766d4220ff43809a4147
emerson-oliveira-clear <emerson.oliveira.clear@gmail.com>
2023-09-16 13:48:27 -0300

    UPDATE: SWITCH and CASE exemples

---
+++ Logica de programação/06 - Switch-Case/index.js
@@ -0,0 +1,19 @@
+
+const respCerta = 'a';
+
+switch(respCerta){
+
+    case 'a':
+        console.log('Errou')
+    break;
+    case 'b':
+        console.log('Errou')
+    break;
+    case 'c':
+        console.log('Acertou')
+    break;
+    case 'd':
+        console.log('Errou')
+    break;
+}
+
