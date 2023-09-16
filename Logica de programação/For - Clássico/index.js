7bc65392c9990d0a5189d09e8d995a52b128ab11
emerson-oliveira-clear <emerson.oliveira.clear@gmail.com>
2023-09-16 18:13:03 -0300

    UPDATE: FOR exemple

---
+++ Logica de programação/For - Clássico/index.js
@@ -0,0 +1,16 @@
+
+const frutas = ['uva', 'pera', 'manga', 'banana']
+
+for( i = 0 ; i < frutas.length ; i++){
+    
+    console.log(`A fruta ${frutas[i]}, está no indice ${i}`);
+}
+   
+console.log('------------------------');
+
+
+for( i = 1 ; i <= 10 ; i++){
+    
+    console.log('Numeros de 1 a 10:',i);
+}
+
