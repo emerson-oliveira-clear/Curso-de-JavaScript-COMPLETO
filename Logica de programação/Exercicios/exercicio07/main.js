ce5203074e4f4051f8fd0bc4f750bd3a6ab6eb15
emerson-oliveira-clear <emerson.oliveira.clear@gmail.com>
2023-09-19 17:56:09 -0300

    CHELLENGER: function FIZZBUZZ

--- Logica de programação/Exercicios/exercicio07/main.js
+++ Logica de programação/Exercicios/exercicio07/main.js
@@ -1,12 +1,37 @@
+let num1 = Math.floor(Math.random() * (100 - 1) + 1)
 
-const paragrafos = document.querySelector('.container');
-const ps = paragrafos.querySelectorAll('p');
 
-const estilosBody = getComputedStyle(document.body)
-const backgroundColorBody = estilosBody.backgroundColor;
-console.log(backgroundColorBody)
-
-for (let p of ps) {
-    p.style.backgroundColor = backgroundColorBody;
-    p.style.color = 'white';
+function Divi3(num1) {
+    if (num1 % 3 == 0) {
+        console.log(' Fizz')
+    }
+    else {
+        console.log('Não é divisil por 3')
+    }
 }
+
+function Divi5(num1) {
+    if (num1 % 5 == 0) { console.log(' Buzz') } else {
+        console.log('Não é divisil por 5')
+    }
+}
+
+function Divi3e5(num1) {
+    if (num1 % 5 == 0 && num1 % 3 == 0) { console.log(' FizzBuzz') } else { console.log(num1) }
+}
+
+function eNum(num1) {
+    if (isNaN(num1) == false) {
+        console.log('é um numero');
+
+    }
+}
+
+function exercicio(num1) {
+    Divi3(num1);
+    Divi5(num1);
+    Divi3e5(num1)
+    eNum(num1)
+}
+
+exercicio(num1);
