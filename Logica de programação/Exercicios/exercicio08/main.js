9ac7120484be9efddd14aa4a2415baf35e7a717d
emerson-oliveira-clear <emerson.oliveira.clear@gmail.com>
2023-09-21 17:59:20 -0300

    CHELLENGER: created a timer

--- Logica de programação/Exercicios/exercicio08/main.js
+++ Logica de programação/Exercicios/exercicio08/main.js
@@ -1,50 +1,45 @@
-
-const btnRelogio = document.querySelector('.Relogio')
+function criandoTimer(){
+    const relogio = document.querySelector('.relogio')
 const btnIniciar = document.querySelector('.Iniciar');
 const btnPausar = document.querySelector('.Pausar');
 const btnZerar = document.querySelector('.Zerar');
 
-
-
-/*INICIAR - inicia do 0 e de onde parar*/
-
-/*PAUSAR - pausa o contador*/
-
-
-/*
-//FUNCAO PARA CRIAR O TIMER
-function criaTimer() {
-    let data = new Date();
+//função para iniciar o time em 00:00:00
+new Date(0)
+function criaHoraDosSegundos(segundos) {
+    const data = new Date(segundos * 1000);
     return data.toLocaleTimeString('pt-BR', {
-        hour12: false
+        hour12: false,
+        timeZone: 'GMT'
     })
 }
 
-const timerInicial = setInterval( e =>{
-   // console.log(criaTimer());
-},1000)
+//função para fazer o timer rodar
+let segundos = 0
+let timer;
 
+function IniciaRelogio() {
+    timer = setInterval(function () {
+        segundos++;
+        relogio.innerHTML = criaHoraDosSegundos(segundos)
+    }, 1000)
+}
 
-//EVENTO DOS BOTOES CAPTURADOS
-btnIniciar.addEventListener('click', e => {
-    //alert('cliquei no iniciar')
-    console.log('chega aqui')
-    btnRelogio.innerHTML = timerInicial
-
+document.addEventListener('click', e => {
+    if (e.target == btnIniciar) {
+        relogio.classList.remove('pausado')
+        clearInterval(timer)
+        IniciaRelogio()
+    } else if (e.target == btnPausar) {
+        clearInterval(timer)
+        relogio.classList.add('pausado')
+    } else if(e.target == btnZerar){
+        clearInterval(timer)
+        relogio.innerHTML = '00:00:00';
+        segundos = 0;
+        relogio.classList.remove('pausado')
+    }
 })
+}
 
-btnPausar.addEventListener('click',e => {
-   // alert('cliquei no pausar')
-    setTimeout(()=>{
-        timerInicial()
-    },    console.log('chega aqui'))
-    
-})
-
-btnZerar.addEventListener('click', e => {
-    alert('cliquei no zerar')
-})
-
-
-
-*/
+criandoTimer();
