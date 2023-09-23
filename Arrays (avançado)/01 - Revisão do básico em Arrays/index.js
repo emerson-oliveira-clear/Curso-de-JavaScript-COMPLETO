99d29a989a73891c5b4a0120eacbee624dc8f453
emerson-oliveira-clear <emerson.oliveira.clear@gmail.com>
2023-09-23 18:31:01 -0300

    UPDATE: basics about arrays

---
+++ Arrays (avançado)/01 - Revisão do básico em Arrays/index.js
@@ -0,0 +1,34 @@
+//                 0        1       2
+const nomes = ['eduardo','maria','joana'];
+const novo = [...nomes]
+
+novo.pop();
+console.log(novo)
+console.log(nomes)
+
+
+
+
+//---------------------------COMEÇO--------------------------------
+
+//SHIFT - RETIRA o elemento do PRIMEIRO indice(e consegue salva-lo em outra variavel)
+
+//UNSHIFT - ADICIONA mais um indice no COMEÇO do array
+
+//---------------------------FINAL--------------------------------
+
+//POP - RETIRA o elemento do ULTIMO indice(e consegue salva-lo em outra variavel)
+
+//PUSH - ADICIONA mais um indice no final do array
+
+//---------------------------SLICE--------------------------------
+
+//pega os indices do array selecionado (tem que ser indices sequenciais)
+
+//---------------------------SPLIT--------------------------------
+
+//separa a string conforme por espaços ou qualquer outro indicativos que escolher
+
+//---------------------------JOIN--------------------------------
+
+//juntas os indices do array em um array só()
