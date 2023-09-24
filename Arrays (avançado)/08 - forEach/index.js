2261fc239f8588bc6278f573815b2e2497a800a9
emerson-oliveira-clear <emerson.oliveira.clear@gmail.com>
2023-09-24 12:28:39 -0300

    UPDATE: forEach exemple

---
+++ Arrays (avançado)/08 - forEach/index.js
@@ -0,0 +1,11 @@
+const numeros = [5,50,80,1,2,3,5,8,7,11,15,22,27];
+
+for( let valor of numeros){
+    console.log(valor)
+}
+console.log('----------forEach----------')
+
+const numeros2 = [1,2,3,4,5,6,7,8,9,10];
+numeros2.forEach(valor => {
+    console.log(valor)
+})
