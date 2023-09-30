3b443fd044b2f4e8a51d3a1522cb3bfed75baadc
emerson-oliveira-clear <emerson.oliveira.clear@gmail.com>
2023-09-30 17:58:45 -0300

    UPDATE: objects review

--- Objetos (avançado)/01 - Revisando Objetos/index.js
+++ Objetos (avançado)/01 - Revisando Objetos/index.js
@@ -1,8 +1,15 @@
-
-function Pessoa(nome, sobrenome) {
-    this.nome =nome,
+function Pessoa (nome,sobrenome){
+    this.nome = nome;
     this.sobrenome = sobrenome
 }
 
-const p1 = new Pessoa('miguel', 'angelo')
-console.log(p1.nome)
+
+const p1 = new Pessoa( 'marcos','leonardo')
+Object.freeze(p1)
+p1.nome = 'outra coisa'
+
+const p2 = new Pessoa( 'gilson','guilherme')
+p1.nome = 'outra coisa'
+
+console.log(p1)
+console.log(p2)
