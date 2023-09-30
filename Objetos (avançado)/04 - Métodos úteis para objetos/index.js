455b6bc731541dadf793bd3c97dac89c270060d6
emerson-oliveira-clear <emerson.oliveira.clear@gmail.com>
2023-09-30 17:59:56 -0300

    UPDATE: some methods for objects

---
+++ Objetos (avançado)/04 - Métodos úteis para objetos/index.js
@@ -0,0 +1,21 @@
+
+
+
+const produto = { nome: 'desodorante', preço: 10}
+newProduto = Object.assign({},produto)
+newProduto.nome = 'Arruda'
+
+console.log(produto)
+console.log(newProduto)
+
+
+
+ 
+/*
+const produto = { nome: 'desodorante', preço: 10}
+newProduto = {...produto} 
+newProduto.nome = 'Arruda'
+
+console.log(produto)
+console.log(newProduto)
+*/
