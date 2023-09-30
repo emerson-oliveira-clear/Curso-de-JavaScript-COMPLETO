b87c87000283150af19200475fe6c5246cc929f5
emerson-oliveira-clear <emerson.oliveira.clear@gmail.com>
2023-09-30 17:59:28 -0300

    UPDATE: getters and setters

--- Objetos (avançado)/03 - Getters e Setters/index.js
+++ Objetos (avançado)/03 - Getters e Setters/index.js
@@ -1,24 +1,39 @@
 function Produto(nome, preco,estoque){
     this.nome = nome,
     this.valor = preco,
-    //this.estoque = estoque
+    this.estoquePrivado = estoque
 
     Object.defineProperty(this, 'estoque' ,{
         enumerable:true, //true mostra a chave
         configurable: true, //pode apagar a chave
         get: function(){
-            return estoque;
+            return estoquePrivado;
         },
         set: function (valor){
-             
+            if(typeof valor !== 'number'){
+                console.log('bad value')
+                return
+            }
+            estoquePrivado = valor
         }
-
     })
-    
+}
+//teste 2
+function criProduto(nome){
+    return{
+        get nomeP(){
+           return nome
+        },
+        set nomeP(valor){
+            nome = valor
+        }
+    }
 }
 
+const p2 = criProduto('casaco')
+console.log(p2)
 
-const p1 = new Produto('Camisa',20,3)
-
-console.log(p1)
-console.log(p1.estoque)
+//const p1 = new Produto('Camisa',20,3)
+//console.log(p1)
+//p1.estoque = 550;
+//console.log(p1.estoque)
