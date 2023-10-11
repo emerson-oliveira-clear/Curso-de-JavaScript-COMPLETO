4c1d1cb0ae21580b89574db4888b4d864cb1b1c1
emerson-oliveira-clear <emerson.oliveira.clear@gmail.com>
2023-10-11 18:45:01 -0300

    UPDATE: Promises

--- JavaScript Assincrono/01 - Promises/index.js
+++ JavaScript Assincrono/01 - Promises/index.js
@@ -2,13 +2,14 @@
 function rand(min,max){
     min *=1000
     max *=1000
-    return Math.floor(Math.random() *(max -min) + min)
+    return Math.floor(Math.random() *(max - min) + min)
 }
 
 
 
 function esperaAi(msg,tempo){
     return new Promise((resolve,reject) =>{
+        if (typeof msg !== "string") reject(new Error ('ERRO'))
         setTimeout(() => {
             resolve(msg);
         }, tempo)
@@ -17,10 +18,22 @@ function esperaAi(msg,tempo){
    
 }
 
-esperaAi('Frase 1 ', rand(1,3))
-.then(resposta => {
-    console.log(resposta)
-})
-.catch()
+esperaAi('frase 1 ', rand(1,3))
+    .then(resposta => {
+        console.log(resposta)
+        return esperaAi('frase 2', rand(1,3))
+    })
+    .then(resposta => {
+        console.log(resposta )
+        return esperaAi(333, rand(1,3))
+    }).then(resposta => {
+        console.log(resposta)
+    }).then(() =>{
+        console.log("enfim consegui entender essa parada")
+    }).catch( e =>{
+        console.log('ERRO:', e)
+    })
+
+
 
 
