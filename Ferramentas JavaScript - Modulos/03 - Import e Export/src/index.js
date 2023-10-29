fabba1d1ceeacef48ab15a18072a6ee893d56216
emerson-oliveira-clear <emerson.oliveira.clear@gmail.com>
2023-10-29 08:36:59 -0300

    UPDATE: import and export exemple

---
+++ Ferramentas JavaScript - Modulos/03 - Import e Export/src/index.js
@@ -0,0 +1,14 @@
+import './assets/css/style.css';
+//import { nome as nome2,sobrenome,idade,soma } from './modulo1' 
+
+//pode tanto importar como exportar com outro nome, usado caso ja tenha uma variavel ou funcao com o mesmo nome
+
+import qualquerNome from './modulo1'
+
+
+const nome = 'joao' 
+
+
+
+//console.log(nome,nome2,sobrenome,idade);
+console.log(qualquerNome(5,8));
---
+++ Ferramentas JavaScript - Modulos/03 - Import e Export/src/modulo1.js
@@ -0,0 +1,9 @@
+const nome = 'luiz';
+const sobrenome = 'miranda';
+export const idade = 30;
+
+export default function soma(x,y){
+    return x + y;
+}
+
+export { nome, sobrenome }
